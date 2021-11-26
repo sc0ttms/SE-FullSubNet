e3a1ea3b5d8f9d1f0a34a0bf6d3fbb530056d831
scott.meng <scott.meng@libratone.com.cn>
2021-11-26 15:57:14 +0800

    perf: update dataset.py

--- audio/feature.py
+++ audio/feature.py
@@ -1,6 +1,36 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
 import paddle
+
+
+def sub_sample(noisy, clean, samples):
+    """random select fixed-length data from noisy and clean
+
+    Args:
+        noisy (float): noisy data
+        clean (float): clean data
+        samples (int): fixed length
+
+    Returns:
+        noisy, clean: fixed-length noisy and clean
+    """
+    length = len(noisy)
+
+    if length > samples:
+        start_idx = np.random.randint(length - samples)
+        end_idx = start_idx + samples
+        noisy = noisy[start_idx:end_idx]
+        clean = clean[start_idx:end_idx]
+    elif length < samples:
+        noisy = np.append(noisy, np.zeros(samples - length))
+        clean = np.append(clean, np.zeros(samples - length))
+    else:
+        pass
+
+    assert len(noisy) == len(clean) == samples
+
+    return noisy, clean
 
 
 def drop_band(input, num_groups=2):
